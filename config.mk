49266057587cbd033fa7d95d0ef4869ae283d65f
Alejandro Mery <amery@geeks.cl>
2020-12-21 01:40:34 +0000

    add db node based on amery/docker-alpine-mariadb
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- config.mk
+++ config.mk
@@ -1,5 +1,10 @@
 TRAEFIK_BRIDGE ?= traefiknet
 NAME ?= wiki
 HOSTNAME ?= $(NAME).docker.localhost
+MYSQL_IMAGE ?= amery/docker-alpine-mariadb
+MYSQL_DATABASE ?= wiki
+MYSQL_USER ?= $(MYSQL_DATABASE)
+MYSQL_PASSWORD ?= user_password
+MYSQL_ROOT_PASSWORD ?= root_password
 NGINX_IMAGE ?= amery/docker-alpine-nginx
 PHP_IMAGE ?= amery/docker-alpine-php7
